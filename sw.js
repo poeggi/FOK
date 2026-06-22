8743c7f8700bdd6d9a4d1d31843ff2140bb89797
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 07:41:15 +0200

    Settings/credits: no accidental exit on left/right; credits exits only on Enter/ESC

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v8-61';
+const CACHE = 'snake-v8-62';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
